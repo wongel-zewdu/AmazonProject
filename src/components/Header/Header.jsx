9e00e39bfc7a6e6ed40ee26c70ecfaa598ad59c6
wongel-zewdu <nenigenet@gmail.com>
2024-07-15 19:20:00 -0600

    create header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import { SlLocationPin } from "react-icons/sl";
+
+import classes from "./Header.module.css";
+const Header = () => {
+  return (
+    <section className={classes.header}>
+      <section className={classes.header__continer}>
+        <div className={classes.header__logo_continer}>
+          <a href="">
+            <img
+              src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+              alt="amazon log"
+            />
+          </a>
+          <div className={classes.header__delivery}>
+            <span>
+              <SlLocationPin />
+            </span>
+            <div>
+              <p>Delivered to</p>
+              <span>Ethiopia</span>
+            </div>
+          </div>
+        </div>
+        <div className={classes.header__search}>
+          <select className={classes.header__select}>
+            <option>All</option>
+          </select>
+          <input type="text" className={classes.header__input} />
+          <FaSearch size={25} className={classes.header__icon} />
+        </div>
+
+        <div className={classes.header__account}>
+          <div className={classes.header__account_border}>
+            <a href="" classes={classes.header__account_image}>
+              <img src="https://pngimg.com/uploads/flags/flags_PNG14592.png"></img>
+            </a>
+            <select className={classes.header__account_select}>
+              <option value="">EN</option>
+            </select>
+          </div>
+          <a href="">
+            <div className={classes.header__account_link}>
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </div>
+          </a>
+          <a href="" className={classes.header__account_para}>
+            <p>Returns</p>
+            <span>& Orders</span>
+          </a>
+          <a href="" className={classes.header__account_cart}>
+          <IoCartOutline size={35} />
+          <span className={classes.header__account_span}>0</span>
+          </a>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Header;
